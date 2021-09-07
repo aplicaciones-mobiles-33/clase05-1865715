463ec42d653169fd9fdf8747651949e82c4ab139
David Vargas <>
2021-09-07 00:25:05 -0500

    Primera App Angular Clase 05

---
+++ mi-primera-app-angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+
+import { AppComponent } from './app.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { PublicacionesComponent } from './publicaciones/publicaciones.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsuariosComponent,
+    PublicacionesComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
